a96ba87b52727ee7235b812b00d8cb1961b713ad
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-09 16:08:41 -0700

    applied some prettier formating for code readibility

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,34 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../model/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // register new user route
 router.post("/register", async (req, res) => {
     try {
-        const { username, email, password, checkpassword } = req.body;
+        const { username, email, password, checkPassword } = req.body;
 
         // input validation
 
-        if (!username || !email || !password || !checkpassword)
-            return res.status(400).json({ msg: 'Please fill out all the fields' });
+        if (!username || !email || !password || !checkPassword)
+            return res
+                .status(400)
+                .json({ msg: "Please fill out all the fields" });
 
         if (username.length < 5)
-            return res.status(400).json({ msg: 'username length must be at leat 5 characters' })
+            return res
+                .status(400)
+                .json({ msg: "username length must be at leat 5 characters" });
 
         if (email.length < 5)
-            return res.status(400).json({ msg: 'email length must be at leat 5 characters' })
+            return res
+                .status(400)
+                .json({ msg: "email length must be at leat 5 characters" });
 
         if (password.length < 5)
-            return res.status(400).json({ msg: 'password length must be at leat 5 characters' })
+            return res
+                .status(400)
+                .json({ msg: "password length must be at leat 5 characters" });
 
-        if (password !== checkpassword)
-            return res.status(400).json({ msg: 'passwords do not match, please provide matching passwords' })
+        if (password !== checkPassword)
+            return res.status(400).json({
+                msg:
+                    "passwords do not match, please provide matching passwords",
+            });
 
         // check for existing user
         const existingUser = await User.findOne({ email: email });
         if (existingUser)
-            return res.status(400).json({ msg: 'account with this email already exists' })
+            return res
+                .status(400)
+                .json({ msg: "account with this email already exists" });
 
         // password encryption
         const salt = await bcrypt.genSalt(10);
@@ -37,13 +50,12 @@ router.post("/register", async (req, res) => {
         const newUser = new User({
             username,
             email,
-            password: passwordHash
-        })
+            password: passwordHash,
+        });
         const savedUser = await newUser.save();
-        res.json(savedUser)
-
+        res.json(savedUser);
     } catch (err) {
-        res.status(500).json(err.message)
+        res.status(500).json(err.message);
     }
 });
 
