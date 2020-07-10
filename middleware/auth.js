413eaf5c2ae024f6585770c3804fa6ba267aaafd
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-10 14:05:48 -0700

    made change to for correction

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,15 +6,15 @@ const auth = (req, res, next) => {
     if (!token)
       return res
         .status(401)
-        .json({ msg: "No authentication token, authorization denied" });
+        .json({ msg: "No authentication token, authorization denied." });
 
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     if (!verified)
       return res
         .status(401)
-        .json({ msg: "Token verification failed, authorization denied" });
+        .json({ msg: "Token verification failed, authorization denied." });
 
-    res.user = verified.id;
+    req.user = verified.id;
     next();
   } catch (err) {
     res.status(500).json({ error: err.message });
